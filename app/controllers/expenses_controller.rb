6df256904a9b570758bfcd83bdbbd9cd49f27c29
Johan <johan225@hotmail.es>
2019-09-16 15:44:27 -0500

    Agrega la funcion buscar

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
-  def index
-     @expenses = Expense.order("date DESC")
-  end
+	def index
+		palabra = "%#{params[:concept]}%"
+		categoria = "%#{params[:category_id]}%"
+		@expenses = Expense.where("concept LIKE ? AND category_id LIKE ?",palabra,categoria)
+	end
 end
