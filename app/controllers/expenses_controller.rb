d84cbe7e899bc3c4ee068997b60247e3a300f79c
Johan <johan225@hotmail.es>
2019-09-17 12:25:07 -0500

    Agrega el gasto del ususario

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,14 +1,10 @@
 class ExpensesController < ActionController::Base
 	before_action :authenticate_user!
   def index
+    @user = User.find(current_user.id)
   	palabra = "%#{params[:concept]}%"
   	categoria = "%#{params[:category_id]}%"
-  	@expenses = Expense.where("concept LIKE ? AND category_id LIKE ?",palabra,categoria)
-  	if params[:concept].present?
-		@expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-	end
-	if params[:category_id].present?
-    	@expenses = @expenses.where("category_id=?", params[:category_id])
-    end
+  	@expenses = Expense.where("concept LIKE ? AND category_id LIKE ? AND user_id=?",palabra,categoria,current_user.id)
+  	
   end
 end
