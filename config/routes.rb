e51e20a41215a1436ebd18f0b7e0d4f04ec86fc5
Johan <johan225@hotmail.es>
2019-09-17 12:02:23 -0500

    Arreglando ruta

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+	root 'expenses#index'
 	devise_for :users
-	devise_scope :user do
-		root 'devise/sessions#new' 
-	end
 	resources :expenses, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
